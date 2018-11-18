5117685992254c59b8fb4b0f0d56a7e70e4a468e
Sander van Beek <lakitna@gmail.com>
2018-11-18 13:00:52 +0100

    Added basic tests for skipped suites and tests

---
+++ test/bdd/skip.js
@@ -0,0 +1,15 @@
+let flag = undefined;
+
+bundle({ behaviour: "default" }, function() {
+    describe.skip("A skipped suite", function() {
+        it("should be skipped", function() {
+            expect(true, 'This test should have been skipped').to.be.false;
+        });
+    });
+
+    describe("A normal suite", function() {
+        it.skip("is skipped", function() {
+            expect(true, 'This test should have been skipped').to.be.false;
+        });
+    });
+});
