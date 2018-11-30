d79706d0af4b2d7ef727fb73fec6db93cd654da9
Sander van Beek <lakitna@gmail.com>
2018-11-30 15:54:16 +0100

    Refactored interfaces to adhere to lint rules

--- interfaces/tdd.js
+++ interfaces/tdd.js
@@ -4,7 +4,7 @@
  * Module dependencies.
  */
 
-var Test = require('mocha/lib/test');
+const Test = require('mocha/lib/test');
 
 /**
  * TDD-bundle interface:
@@ -33,78 +33,97 @@ var Test = require('mocha/lib/test');
  *
  * @param {Suite} suite Root suite.
  */
-module.exports = function (suite) {
-  var suites = [suite];
+module.exports = function(suite) {
+    const suites = [suite];
 
-  suite.on('pre-require', function (context, file, mocha) {
-    var common = require('mocha/lib/interfaces/common')(suites, context, mocha);
+    suite.on('pre-require', function(context, file, mocha) {
+        const common = require('mocha/lib/interfaces/common')(suites, context, mocha);
 
-    context.setup = common.beforeEach;
-    context.teardown = common.afterEach;
-    context.suiteSetup = common.before;
-    context.suiteTeardown = common.after;
-    context.run = mocha.options.delay && common.runWithSuite(suite);
+        context.setup = common.beforeEach;
+        context.teardown = common.afterEach;
+        context.suiteSetup = common.before;
+        context.suiteTeardown = common.after;
+        context.run = mocha.options.delay && common.runWithSuite(suite);
 
-    context.bundle = require("./bundle")(common, suites, file, 'setup', 'teardown');
+        context.bundle = require('./bundle')(common, suites, file, 'setup', 'teardown');
 
-    /**
-     * Describe a "suite" with the given `title` and callback `fn` containing
-     * nested suites and/or tests.
-     */
-    context.suite = function (title, fn) {
-      return common.suite.create({
-        title: title,
-        file: file,
-        fn: fn
-      });
-    };
+        /**
+         * Describe a 'suite' with the given `title` and callback `fn` containing
+         * nested suites and/or tests.
+         * @param {string} title
+         * @param {function} fn
+         *
+         * @return {Suite}
+         */
+        context.suite = function(title, fn) {
+            return common.suite.create({
+                title: title,
+                file: file,
+                fn: fn,
+            });
+        };
 
-    /**
-     * Pending suite.
-     */
-    context.suite.skip = function (title, fn) {
-      return common.suite.skip({
-        title: title,
-        file: file,
-        fn: fn
-      });
-    };
+        /**
+         * Pending suite.
+         * @param {string} title
+         * @param {function} fn
+         *
+         * @return {Suite}
+         */
+        context.suite.skip = function(title, fn) {
+            return common.suite.skip({
+                title: title,
+                file: file,
+                fn: fn,
+            });
+        };
 
-    /**
-     * Exclusive test-case.
-     */
-    context.suite.only = function (title, fn) {
-      return common.suite.only({
-        title: title,
-        file: file,
-        fn: fn
-      });
-    };
+        /**
+         * Exclusive test-case.
+         * @param {string} title
+         * @param {function} fn
+         *
+         * @return {Suite}
+         */
+        context.suite.only = function(title, fn) {
+            return common.suite.only({
+                title: title,
+                file: file,
+                fn: fn,
+            });
+        };
 
-    /**
-     * Describe a specification or test-case with the given `title` and
-     * callback `fn` acting as a thunk.
-     */
-    context.test = function (title, fn) {
-      var suite = suites[0];
-      if (suite.isPending()) {
-        fn = null;
-      }
-      var test = new Test(title, fn);
-      test.file = file;
-      suite.addTest(test);
-      return test;
-    };
+        /**
+         * Describe a specification or test-case with the given `title` and
+         * callback `fn` acting as a thunk.
+         * @param {string} title
+         * @param {function} fn
+         *
+         * @return {Test}
+         */
+        context.test = function(title, fn) {
+            const suite = suites[0];
+            if (suite.isPending()) {
+                fn = null;
+            }
+            const test = new Test(title, fn);
+            test.file = file;
+            suite.addTest(test);
+            return test;
+        };
 
-    /**
-     * Exclusive test-case.
-     */
+        /**
+         * Exclusive test-case.
+         * @param {string} title
+         * @param {function} fn
+         *
+         * @return {Test}
+         */
+        context.test.only = function(title, fn) {
+            return common.test.only(mocha, context.test(title, fn));
+        };
 
-    context.test.only = function (title, fn) {
-      return common.test.only(mocha, context.test(title, fn));
-    };
-
-    context.test.skip = common.test.skip;
-    context.test.retries = common.test.retries;
-  });
+        context.test.skip = common.test.skip;
+        context.test.retries = common.test.retries;
+    });
 };
